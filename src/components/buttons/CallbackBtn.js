1de36d6202d9e08f737fcb4c4b653f6b1fa74d9a
Noor Mohammad <noormohammad@AAOMAN.COM>
2022-10-16 12:46:11 +0400

    committed for the first time

---
+++ src/components/buttons/CallbackBtn.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CallbackBtn = () => {
+  return (
+    <div>
+      <button className="bg-gradient-to-r from-[#004165] via-[#772432] to-[#a9b2b1] font-titleFont px-5 py-3 rounded-md text-white text-[16px] hover:bg-gradient-to-l  duration-300">
+        Request to callback
+      </button>
+    </div>
+  );
+  // from-[#004165] via-[#f2df74] to-[#772432]
+};
+
+export default CallbackBtn;
---
+++ src/components/buttons/ReadmoreBtn.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ReadmoreBtn = () => {
+  return (
+    <div>
+      <button className="bg-gradient-to-r from-[#004165] via-[#772432] to-[#a9b2b1] font-titleFont px-5 py-2 rounded-md text-white text-[16px] hover:bg-gradient-to-l  duration-300">
+        Read More
+      </button>
+    </div>
+  );
+};
+
+export default ReadmoreBtn;
