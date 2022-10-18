b849f7b03b44584923fa4a591c885d2b06981577
Noor_Mohammad <unknown2832021@gmail.com>
2022-10-18 23:49:40 +0400

    screenshot done

--- src/components/slider/PrimeSlider.js
+++ src/components/slider/PrimeSlider.js
@@ -10,7 +10,7 @@ import {
 
 const PrimeSlider = () => {
   return (
-    <div className="w-full h-[500px] mb-20 flex flex-col lgl:flex-row justify-center gap-2 items-center">
+    <div className="w-full max-h-[500px] mb-20 py-20 flex flex-col lgl:flex-row justify-center gap-2 items-center">
       <div className="w-full lgl:w-1/2 px-4 lgl:px-0">
         <Carousel
           className="w-full h-full"
@@ -59,15 +59,20 @@ const PrimeSlider = () => {
           </div>
         </Carousel>
       </div>
-      <div className="w-full lgl:w-1/2 h-[450px] px-10 flex flex-col">
+      <div className="w-full lgl:w-1/2 h-full flex flex-col pt-20 px-10 gap-6">
         <h1 className="text-3xl font-extrabold bg-clip-text text-transparent bg-gradient-to-t from-[#004165] via-[#772432] to-[#772432]">
           Some of our Leaders
         </h1>
-        <p className="text-[#434343] text-base leading-7">
+        <p className="text-[#434343] text-lg font-normal">
           Midas Toastmaster Club is Lorem ipsum dolor sit amet consectetur
           adipisicing elit. Nulla iusto vero possimus eius velit, explicabo
           cupiditate aliquam consequuntur at illum. Lorem ipsum dolor sit amet
-          lorem consequer.
+          lorem consequer. Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Fugit voluptatum reiciendis, quod iste voluptates voluptas quae
+          suscipit quos molestias. Ut saepe at minima nihil doloribus maxime
+          veniam aut iure totam. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Possimus corrupti, corporis magnam sequi ut ducimus
+          obcaecati atque? Quis, quibusdam veritatis.
         </p>
       </div>
     </div>
